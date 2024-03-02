da7d19d4514eaecedbbb043f0f46434ce5c1abc2
Raxmatilla <56291918+Raxmatilla97@users.noreply.github.com>
2024-03-02 14:13:17 +0500

    yan

--- resources/views/dashboard/asosiy-sahifa/card.blade.php
+++ resources/views/dashboard/asosiy-sahifa/card.blade.php
@@ -1,13 +1,13 @@
 
 
-<div class="w-full max-w-xl bg-white border border-gray-200 rounded-lg shadow ">
+<div class="w-full max-w-md bg-white border border-gray-200 rounded-lg shadow ">
     <div class="flex justify-end px-4 pt-8">
      
       
         
     </div>
     <div class="flex flex-col items-center pb-3">
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{'/storage/users/image'}}/{{$auth->image}}" alt="Bonnie image"/>
+        <img class="w-34 h-34 mb-3 rounded-full shadow-lg" src="{{'/storage/users/image'}}/{{$auth->image}}" alt="Bonnie image"/>
         <h5 class="mb-1 text-xl font-medium text-gray-900 ">{{ $auth->second_name}} {{ $auth->first_name}}</h5>
         <span class="text-sm text-gray-500">#ID {{$auth->employee_id_number}}</span>
       
--- resources/views/dashboard/asosiy-sahifa/forma2.blade.php
+++ resources/views/dashboard/asosiy-sahifa/forma2.blade.php
@@ -9,11 +9,11 @@
 
 
 
-<div class="max-w-xl w-full space-y-8 p-10 border border-gray-200 rounded-lg shadow z-10">
+<div class="max-w-xxl w-full space-y-8 p-10 border border-gray-200 rounded-lg shadow z-10">
     <div class="grid  gap-8 grid-cols-1">
         <div class="flex flex-col ">
             <div class="flex flex-col sm:flex-row items-center">
-                <h2 class="font-semibold text-lg mr-auto">Reyting uchun ma'lumot joylash uchun kerakli yo'nalishni tanlang</h2>
+                <h2 class="font-semibold text-lg mr-auto">Reyting ma'lumotini joylash uchun kerakli yo'nalishni tanlang</h2>
                 <div class="w-full sm:w-auto sm:ml-auto mt-3 sm:mt-0"></div>
             </div>
             <div class="flex items-center p-4 mt-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
@@ -25,25 +25,27 @@
                   <span class="font-medium">Etibor bering!</span>Ma'lumot yuklashdan oldin uning yo'nalishini belgilang.
                 </div>
               </div>
-            <div class="mt-2">
-                <div class="form mb-12">
-
-                    <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ma'lumot yuborish yo'nalishi</label>                  
-                    <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option selected>Yo'nalishni tanlang</option>
-                        @foreach($categoryes as $category)                     
-                      <option value="{{$category->id}}">{{$category->name}}</option>
-                     
-                      @endforeach
-                    </select>
-                    <div class="mt-8 text-right md:space-x-3 md:block flex flex-col-reverse">
-                      
-                        <button type="submit"
-                            class="mb-2 md:mb-0 bg-green-400 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-green-500">Yuborish</button>
-                    </div>
-
-
-                </div>
-            </div>
+              
         </div>
+        <div class=" flex justify-start w-full ">
+            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/1fcafd93322439.5e6164ef9bb7f.jpg" alt="">
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">O'qituvchilar tomonidan yuboriladigan ma'lumotlar uchun</h5>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Universitet o'qituvchi hodimlari tomonidan ilmiy yo'nalishdagi ma'lumotlarni yuborishlari uchun</p>
+                </div>
+            </a>
+        </div>
+        <div class="mt-2 flex justify-start">
+            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://i.pinimg.com/736x/f4/b2/e9/f4b2e9d31c338e7bc4efd5c83f5af10f.jpg" alt="">
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Kafedra ma'lumotlarini yuborish uchun</h5>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Kafedra ma'lumotlarini yuborish uchun </p>
+                </div>
+            </a>
+        </div>
+        
     </div>
+
+</div>
