3a93eca59b57972a5027852a525ecfc5ef7944ec
Raxmatilla Fayziyev <56291918+Raxmatilla97@users.noreply.github.com>
2024-08-21 11:53:25 +0500

    Yangilash

--- resources/views/dashboard/incoming_requests.blade.php
+++ resources/views/dashboard/incoming_requests.blade.php
@@ -334,7 +334,7 @@
                                             {{  $item->created_at->diffForHumans() }}
                                         </td>
                                         <td class="px-6 py-4">
-                                            @if(Auth::user()->id == 16)
+                                            @if(Auth::user()->id == 17)
 
                                                 <button data-tooltip-target="tooltip-default" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 text-sm dark:focus:ring-blue-800">Ko'rish</button>
 
