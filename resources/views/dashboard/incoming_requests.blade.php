399cc3d0d8cbdbf0159ca24732a8b258eba99ea1
Raxmatilla Fayziyev <56291918+Raxmatilla97@users.noreply.github.com>
2024-08-21 11:52:14 +0500

    Yangilash

--- resources/views/dashboard/incoming_requests.blade.php
+++ resources/views/dashboard/incoming_requests.blade.php
@@ -334,16 +334,17 @@
                                             {{  $item->created_at->diffForHumans() }}
                                         </td>
                                         <td class="px-6 py-4">
-                                            @if(Auth::user()->id != 16)
+                                            @if(Auth::user()->id == 16)
+
+                                                <button data-tooltip-target="tooltip-default" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 text-sm dark:focus:ring-blue-800">Ko'rish</button>
+
+                                                <div id="tooltip-default" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                                    Sizga ma'lumotlarga ball berish huquqi berilmagan!
+                                                    <div class="tooltip-arrow" data-popper-arrow></div>
+                                                </div>
+                                            @else
                                             <a href="{{ route('murojatlar.show', ['id' => $item->id]) }}"
                                                 class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Ko'rish</a>
-                                            @else
-                                            <button data-tooltip-target="tooltip-default" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 text-sm dark:focus:ring-blue-800">Ko'rish</button>
-
-                                            <div id="tooltip-default" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                                                Sizga ma'lumotlarga ball berish huquqi berilmagan!
-                                                <div class="tooltip-arrow" data-popper-arrow></div>
-                                            </div>
                                             @endif
                                         </td>
                                     </tr>
