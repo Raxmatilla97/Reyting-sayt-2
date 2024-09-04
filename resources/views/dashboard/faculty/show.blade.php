1f7bef60e519de60634ba9d31e35dafa3cc72542
Raxmatilla Fayziyev <56291918+Raxmatilla97@users.noreply.github.com>
2024-09-04 13:53:02 +0500

    Yangilash

--- resources/views/dashboard/faculty/show.blade.php
+++ resources/views/dashboard/faculty/show.blade.php
@@ -334,73 +334,38 @@
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    @foreach ($faculty->departments as $item)
-                                                        <tr class="border-b">
-
-                                                            <td class="px-4 py-4">
-
-                                                                <span
-                                                                    class="inline-flex items-center justify-center w-5 h-5 me-2 text-md font-semibold text-blue-800 bg-blue-100 rounded-full dark:bg-gray-700 dark:text-blue-400">
-                                                                    <svg class="w-3 h-3" aria-hidden="true"
-                                                                        xmlns="http://www.w3.org/2000/svg"
-                                                                        fill="currentColor" viewBox="0 0 20 20">
-                                                                        <path fill="currentColor"
-                                                                            d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z">
-                                                                        </path>
-                                                                        <path fill="#fff"
-                                                                            d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z">
-                                                                        </path>
-                                                                    </svg>
-                                                                </span>
-
-                                                                <span
-                                                                    class="bg-blue-100 text-blue-800 text-md font-medium px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-                                                                    <a
-                                                                        href="{{ route('dashboard.departmentShow', ['slug' => $item->slug]) }}">{{ $item->name }}</a>
-
-                                                                </span>
-                                                            </td>
-
-                                                            <td class="px-4 py-2 text-center">
-                                                                {{ $item->user->count() }}
-                                                            </td>
-
-                                                            <td class="px-4 py-2 text-center">
-
-                                                                @php
-                                                                    $totalInfos = 0;
-                                                                    if ($item->department) {
-                                                                        $totalInfos = $item->department
-                                                                            ->point_user_deportaments()
-                                                                            ->where('user_id', $item->id)
-                                                                            ->count();
-                                                                    }
-                                                                @endphp
-
-                                                                {{ $totalInfos }}
-                                                            </td>
-                                                            <td class="px-4 py-2 text-center">
-                                                                @php
-                                                                $point_full = 0;
-                                                                if ($item->department) {
-                                                                    $points = $item->department
-                                                                        ->point_user_deportaments()
-                                                                        ->where('status', 1)
-                                                                        ->where('user_id', $item->id)
-                                                                        ->get();
-
-                                                                    foreach ($points as $point) {
-                                                                        $point_full += $point->point;
-                                                                    }
-                                                                }
+                                                    @foreach ($faculty->departments as $department)
+                                                    <tr class="border-b">
+                                                        <td class="px-4 py-4">
+                                                            <span class="inline-flex items-center justify-center w-5 h-5 me-2 text-md font-semibold text-blue-800 bg-blue-100 rounded-full dark:bg-gray-700 dark:text-blue-400">
+                                                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                                                    <path fill="currentColor" d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z"></path>
+                                                                    <path fill="#fff" d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z"></path>
+                                                                </svg>
+                                                            </span>
+                                                            <span class="bg-blue-100 text-blue-800 text-md font-medium px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                                                                <a href="{{ route('dashboard.departmentShow', ['slug' => $department->slug]) }}">{{ $department->name }}</a>
+                                                            </span>
+                                                        </td>
+                                                        <td class="px-4 py-2 text-center">
+                                                            {{ $department->user->count() }}
+                                                        </td>
+                                                        <td class="px-4 py-2 text-center">
+                                                            @php
+                                                                $totalInfos = $department->point_user_deportaments->count();
                                                             @endphp
-
+                                                            {{ $totalInfos }}
+                                                        </td>
+                                                        <td class="px-4 py-2 text-center">
+                                                            @php
+                                                                $point_full = $department->point_user_deportaments
+                                                                    ->where('status', 1)
+                                                                    ->sum('point');
+                                                            @endphp
                                                             {{ $point_full }}
-
-
-                                                            </td>
-                                                        </tr>
-                                                    @endforeach
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
                                                     <!-- Qo'shimcha satrlar qo'shishingiz mumkin -->
                                                 </tbody>
                                             </table>
