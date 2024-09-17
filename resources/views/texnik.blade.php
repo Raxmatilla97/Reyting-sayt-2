d2397dbe92f21990171c97ab77ee67643cc005b9
Raxmatilla Fayziyev <56291918+Raxmatilla97@users.noreply.github.com>
2024-09-17 18:10:46 +0500

    Yangilanish

--- resources/views/texnik.blade.php
+++ resources/views/texnik.blade.php
@@ -1,17 +1,34 @@
-<div id="modal" class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full hidden flex items-center justify-center" style="z-index: 9999;">
+<div id="modal"
+    class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full hidden flex items-center justify-center"
+    style="z-index: 9999;">
     <div class="relative p-8 border w-11/12 max-w-2xl shadow-lg rounded-md bg-white">
         <div class="mt-3 text-center">
             <i class="fas fa-exclamation-circle text-5xl text-yellow-500 mb-4"></i>
             <h3 class="text-2xl leading-6 font-bold text-gray-900 mb-4">Muhim xabar</h3>
             <div class="mt-2 px-7 py-3">
-                <p class="text-lg text-gray-700 leading-relaxed mb-5">
-                    Hurmatli foydalanuvchilar, saytimizda O'qituvchi, Kafedra va Fakultet ballarini shakllantirish tizimi yangilandi. Hozirda barcha ma'lumotlar uchun ballar shakllantirilmoqda. Agar tizimda biror xatolik yoki noaniqlik sezsangiz, iltimos, bizga xabar bering. Sizning fikr-mulohazalaringiz bizga tizimni yanada takomillashtirish imkonini beradi.
-                </p>
-                <a href="https://t.me/Raxmatilla_Fayziyev" target="_blank" rel="noopener noreferrer" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Telegram orqali xabar berish uchun!</a>
+                <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50" role="alert">
+                    <span class="font-medium">16.09.2024</span> Hurmatli foydalanuvchilar, saytimizda O'qituvchi,
+                    Kafedra va Fakultet ballarini shakllantirish tizimi yangilandi. Hozirda barcha ma'lumotlar uchun
+                    ballar shakllantirilmoqda. Agar tizimda biror xatolik yoki noaniqlik sezsangiz, iltimos, bizga xabar
+                    bering. Sizning fikr-mulohazalaringiz bizga tizimni yanada takomillashtirish imkonini beradi.
+                </div>
+                <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50" role="alert">
+                    <span class="font-medium">17.09.2024</span> Hurmatli foydalanuvchilar, saytimizda yangi yo'nalishlar
+                    qo'shildi! table_1_3_1_a_, table_1_3_1_b_ va table_1_3_2_a_, table_1_3_2_b_ lar uchun ma'lumotlarni
+                    jo'natishingiz mumkin.
+                </div>
+                <div class="text-right">
+                    <a href="https://t.me/Raxmatilla_Fayziyev" target="_blank" rel="noopener noreferrer"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    Xatolik topilsa!
+                </a>
+                </div>
+
 
             </div>
             <div class="items-center px-4 py-3">
-                <button id="closeModal" class="px-6 py-3 bg-blue-500 text-white text-lg font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 transition duration-300">
+                <button id="closeModal"
+                    class="px-6 py-3 bg-blue-500 text-white text-sm font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 transition duration-300">
                     Tushundim
                 </button>
             </div>
