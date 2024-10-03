022b58f099746be0f39344ed56c3e77971f4d65a
Raxmatilla Fayziyev <56291918+Raxmatilla97@users.noreply.github.com>
2024-10-03 13:06:02 +0500

    Update department_category_choose.blade.php

--- resources/views/dashboard/department_category_choose.blade.php
+++ resources/views/dashboard/department_category_choose.blade.php
@@ -27,7 +27,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                {{-- @foreach ($jadvallar_codlari as $key => $volume)
+                                @foreach ($jadvallar_codlari as $key => $volume)
                                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                         <th scope="row"
                                             class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white hide-on-mobile">
@@ -44,9 +44,9 @@
                                         </td>
 
                                     </tr>
-                                @endforeach --}}
+                                @endforeach 
 
-                                <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+<!--                                 <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
                                     <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
                                     </svg>
@@ -54,7 +54,7 @@
                                     <div>
                                       <span class="font-medium">Diqqat!</span> Ma'lumot yuklash muddati o'z nihoyasiga yetdi! 
                                     </div>
-                                  </div>
+                                  </div> -->
                                   
                             </tbody>
                         </table>
