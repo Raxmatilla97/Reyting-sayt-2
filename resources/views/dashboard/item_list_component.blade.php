0ba48b58dc59309c83d6cfe96377a7b5d8f1459a
Raxmatilla Fayziyev <56291918+Raxmatilla97@users.noreply.github.com>
2024-11-16 17:18:36 +0500

    Yangilanish

--- resources/views/dashboard/item_list_component.blade.php
+++ resources/views/dashboard/item_list_component.blade.php
@@ -215,12 +215,12 @@
                 <div class="flex items-center gap-2">
                     <div id="edit-button-container" style="display: none;">
                         <a href="#" id="edit-link"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 flex items-center gap-2">
+                            class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-4 py-2 flex items-center gap-2">
                             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                             </svg>
-                            Tahrirlash
+                            Qayta yuborish
                         </a>
                     </div>
                     <button type="button"
